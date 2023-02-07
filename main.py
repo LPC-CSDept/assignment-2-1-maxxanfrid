9e7574955be3b3db62a8b307ddf19facb2c8b01a
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-06 16:12:08 -0800

    02-1

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
     ##################################################
     num_males = int(input('Enter your number'))
     num_females = int(input('Enter your number'))
-    total = num_males + num_females 
-    perc_males = float(num_males)/total
-    perc_females = float(num_females)/total
+    total = num_males + num_females; 
+    perc_males = float(num_males)/total; 
+    perc_females = float(num_females)/total; 
     print(total)
     print(num_males), print(num_females)
     print(perc_males); print(perc_females)
