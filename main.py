d957233a90c4fd44482792d09d739c8d829fd0b1
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-07 17:04:22 -0800

    assignment-2-1

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ def main():
     ##################################################
     num_males = int(input('Enter your number'))
     num_females = int(input('Enter your number'))
-    total = num_males + num_females; 
-    perc_males = float(num_males)/total; 
-    perc_females = float(num_females)/total; 
-    print(total)
-    print(num_males), print(num_females)
-    print(perc_males); print(perc_females)
+    perc_males = num_males/(num_males + num_females) * 100; 
+    perc_females = num_females/(num_males + num_females) * 100
+    print('Total:', num_males + num_females)
+    print('Number of males and females', num_males, num_females)
+    print(f'Percentage of males and females \t {perc_males:.2f}%, \t {perc_females:.2f}%')
     pass
 
 
